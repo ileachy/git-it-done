3e79edaad914fa7f3ee80fc942daf40fb78376cb
Ian L <ileach81@gmail.com>
2021-12-08 03:42:23 -0500

    fetch user repo array

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-const getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
+const getUserRepos = function (user) {
+  // format the github api url
+  const apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
 
 getUserRepos();
