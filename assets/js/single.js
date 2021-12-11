a722ea194c04677047221b7b9e11043ec88c69cf
Ian L <ileach81@gmail.com>
2021-12-11 01:55:45 -0500

    quick save before close

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,12 +9,27 @@ var getRepoIssues = function (repo) {
     if (response.ok) {
       response.json().then(function (data) {
         displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with your request!");
     }
   });
   console.log(repo);
+};
+
+var displayWarning = function (repo) {
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See more issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  //add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
 };
 
 var displayIssues = function (issues) {
@@ -53,4 +69,4 @@ var displayIssues = function (issues) {
   }
 };
 
-getRepoIssues("facebook/react");
+getRepoIssues();
